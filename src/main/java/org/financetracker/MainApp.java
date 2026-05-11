7981337bc18088ad731d2d1d56a7bfb19211079d
olisa kenneth <olisaemekatochukwu@gmail.com>
2026-05-11 01:02:23 -0600

    added JavaFX setup and blank window
    - added javafx-controls and javafx-fxml dependencies to pom.xml
    - added javafx-maven-plugin to build
    - created MainApp.java with stage and launch

---
+++ src/main/java/org/financetracker/MainApp.java
@@ -0,0 +1,16 @@
+package org.financetracker;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class MainApp extends Application {
+    public  void start(Stage stage){
+        stage.setTitle("Finance Tracker");
+        stage.show();
+
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
