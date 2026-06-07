c10803cae9ff345f4614b0adf17b487bf1f91fc0
olisa kenneth <olisaemekatochukwu@gmail.com>
2026-06-07 08:06:57 -0600

    Add global CORS configuration to allow React frontend access

---
+++ financetracker-api/src/main/java/org/financetracker/financetracker_api/WebConfig.java
@@ -0,0 +1,16 @@
+package org.financetracker.financetracker_api;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer{
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**") // which endpoints to cover
+                .allowedOrigins("http://localhost:5173"); // who is allowed in
+    }
+
+}
