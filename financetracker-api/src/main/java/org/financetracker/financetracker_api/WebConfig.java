f16e85acff7697e8f49fdcb1af7e9da8de38b19c
olisa kenneth <olisaemekatochukwu@gmail.com>
2026-06-08 04:17:44 -0600

    Fix CORS config to allow DELETE and PUT methods

--- financetracker-api/src/main/java/org/financetracker/financetracker_api/WebConfig.java
+++ financetracker-api/src/main/java/org/financetracker/financetracker_api/WebConfig.java
@@ -10,7 +10,7 @@ public class WebConfig implements WebMvcConfigurer{
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**") // which endpoints to cover
-                .allowedOrigins("http://localhost:5173"); // who is allowed in
+                .allowedOrigins("http://localhost:5173") // who is allowed in
                 .allowedMethods("GET", "POST", "PUT", "DELETE");
     }
 
